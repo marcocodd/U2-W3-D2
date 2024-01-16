4ae1cb04ce3a70128be18e331cd69cb416524fcd
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-01-16 17:40:29 +0100

    piccole modifiche nel primo ex

--- index.js
+++ index.js
@@ -4,36 +4,30 @@
 const inputAccount = document.getElementById('inputAccount')
 const saveButton= document.getElementById('saveButton')
 const deleteButton=document.getElementById('deleteButton')
+const savedAccountShow = document.getElementById("savedAccount");
 
+saveButton.addEventListener("click", function () {
+ const accountName = inputAccount.value;
+ console.log(accountName);
+ localStorage.setItem("account-name", accountName);
+ alert("nome salvato");
+});
 
+deleteButton.addEventListener("click", function () {
+ localStorage.removeItem("account-name");
+ savedAccountShow.innerText = "";
+ alert("account eliminato");
+});
 
-
-saveButton.addEventListener('click', function(){
-    const accountName= inputAccount.value
-    console.log(accountName)
-    localStorage.setItem('account-name', accountName)
-    alert('nome salvato')
-})
-
-
-deleteButton.addEventListener('click', function(){
- 
-    inputAccount.value=''
-
-    localStorage.removeItem('account-name')
-
-    alert('account eliminato')
-
-})
-
-
-if (localStorage.getItem('account-name')) {
-    const showText = localStorage.getItem('account-name')
-    inputAccount.value= showText
-}
-
-
-
+const ShowSavedAccount = function () {
+ if (localStorage.getItem("account-name")) {
+  // const showText = localStorage.getItem("account-name");
+  savedAccountShow.innerText = localStorage.getItem("account-name");
+ } else {
+  savedAccountShow.innerText = "Non ci sono dati salvati";
+ }
+};
+ShowSavedAccount();
 
 // Esercizio 2
 
